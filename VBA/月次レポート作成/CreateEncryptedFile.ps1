5a1c0ee2408179056c2537465e48f52204aaf231
tsusys <ssktuy@gmail.com>
2026-05-09 11:24:49 +0900

    Add files via upload

---
+++ VBA/月次レポート作成/CreateEncryptedFile.ps1
@@ -0,0 +1,13 @@
+﻿<#
+.SYNOPSIS
+    encrypted.txt を作成する
+
+.DESCRIPTION
+    Box APIで使用するClient Secretを暗号化し、encrypted.txt を作成します。
+#>
+$path = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $path
+
+$cs = Read-Host "クライアントシークレットを入力してください"
+$ss = ConvertTo-SecureString -string $cs -AsPlainText -Force
+$ss | ConvertFrom-SecureString | Out-File encrypted.txt
